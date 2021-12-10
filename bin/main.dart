9ee2f45b537684a8da30624d8166b264ebfc01b9
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2021-12-10 20:41:32 +0100

    :sparkles: (Prettify main.dart)

--- bin/main.dart
+++ bin/main.dart
@@ -17,66 +17,118 @@ import '../src/advent_of_code_8/data_source.dart';
 import '../src/advent_of_code_9/advent_of_code_9.dart';
 import '../src/advent_of_code_9/data_source.dart';
 
+const today = 9;
+
+void displayAdventOfCodeResponse(
+    {required int day, dynamic part1Response, dynamic part2Response}) async {
+  print("📆 Day $day :");
+  print('Part 1: $part1Response');
+  print('Part 2: $part2Response');
+  print("--------------------------------------------------------");
+}
+
+Future<void> runAdventOfCodeProgram({int? day}) async {
+  switch (day) {
+    case 1:
+      final input = await getInput1Data();
+      displayAdventOfCodeResponse(
+        day: 1,
+        part1Response: countIncreasingMeasurements(input),
+        part2Response:
+            countIncreasingMeasurementsByWindow(input, windowSize: 3),
+      );
+      break;
+    case 2:
+      final input = await getInput2Data();
+      displayAdventOfCodeResponse(
+        day: 2,
+        part1Response:
+            getCoordinatesProductForFinalPosition(moveCommands: input),
+        part2Response: getCoordinatesProductForFinalPosition(
+          moveCommands: input,
+          interpretationMethod: CommandInterpretationMethod.aim,
+        ),
+      );
+      break;
+    case 3:
+      final input = await getInput3Data();
+      displayAdventOfCodeResponse(
+        day: 3,
+        part1Response: getPowerConsumption(report: input),
+        part2Response: getLifeSupportRating(report: input),
+      );
+      break;
+    case 4:
+      final input = await getInput4Data();
+      displayAdventOfCodeResponse(
+        day: 4,
+        part1Response: getBingoScore(
+            bingoBoards: input.bingoBoards.map(parseInputBoard).toList(),
+            drawnNumbers: input.drawnNumbers),
+        part2Response: getBingoScore(
+            bingoBoards: input.bingoBoards.map(parseInputBoard).toList(),
+            drawnNumbers: input.drawnNumbers,
+            which: BingoBoardWinningPlace.last),
+      );
+      break;
+    case 5:
+      final input = await getInput5Data();
+      displayAdventOfCodeResponse(
+        day: 5,
+        part1Response:
+            getDangerousAreasMapping(input, size: 1000, excludeDiagonals: true),
+        part2Response: getDangerousAreasMapping(input, size: 1000),
+      );
+      break;
+    case 6:
+      final input = await getInput6Data();
+      displayAdventOfCodeResponse(
+        day: 6,
+        part1Response: simulateLanternFishProduction(input, days: 80),
+        part2Response: simulateOptimizedLanternFishProduction(input, days: 256),
+      );
+      break;
+    case 7:
+      final input = await getInput7Data();
+      displayAdventOfCodeResponse(
+        day: 7,
+        part1Response: getFuelConsumption(input),
+        part2Response: getDynamicFuelConsumption(input),
+      );
+      break;
+    case 8:
+      final input = await getInput8Data();
+      displayAdventOfCodeResponse(
+        day: 8,
+        part1Response: countSimpleDigits(input),
+        part2Response: sumAllDecodedOutputValues(input),
+      );
+      break;
+    case 9:
+      final input = await getInput9Data();
+      displayAdventOfCodeResponse(
+        day: 9,
+        part1Response: sumMinHeights(input),
+        part2Response: getLargestBassinArea(input),
+      );
+      break;
+    default:
+      for (var _day = 1; _day <= today; _day++) {
+        await runAdventOfCodeProgram(day: _day);
+      }
+      break;
+  }
+}
+
 void main(List<String> arguments) async {
-  print("Day 1 :");
-  final input1 = await getInput1Data();
-  print('Part 1: ${countIncreasingMeasurements(input1)}');
-  print(
-      'Part 2: ${countIncreasingMeasurementsByWindow(input1, windowSize: 3)}');
-  print("--------------------------------------------------------");
+  if (arguments.isEmpty) {
+    return runAdventOfCodeProgram();
+  }
 
-  print("Day 2 :");
-  final input2 = await getInput2Data();
-  print(
-      'Part 1: ${getCoordinatesProductForFinalPosition(moveCommands: input2)}');
-  print('Part 2: ${getCoordinatesProductForFinalPosition(
-    moveCommands: input2,
-    interpretationMethod: CommandInterpretationMethod.aim,
-  )}');
-  print("--------------------------------------------------------");
-
-  print("Day 3 :");
-  final input3 = await getInput3Data();
-  print('Part 1: ${getPowerConsumption(report: input3)}');
-  print('Part 2: ${getLifeSupportRating(report: input3)}');
-  print("--------------------------------------------------------");
-
-  print("Day 4 :");
-  final input4 = await getInput4Data();
-  print(
-      'Part 1: ${getBingoScore(bingoBoards: input4.bingoBoards.map(parseInputBoard).toList(), drawnNumbers: input4.drawnNumbers)}');
-  print(
-      'Part 2: ${getBingoScore(bingoBoards: input4.bingoBoards.map(parseInputBoard).toList(), drawnNumbers: input4.drawnNumbers, which: BingoBoardWinningPlace.last)}');
-  print("--------------------------------------------------------");
-
-  print("Day 5 :");
-  final input5 = await getInput5Data();
-  print(
-      'Part 1: ${getDangerousAreasMapping(input5, size: 1000, excludeDiagonals: true)}');
-  print('Part 2: ${getDangerousAreasMapping(input5, size: 1000)}');
-  print("--------------------------------------------------------");
-
-  print("Day 6 :");
-  final input6 = await getInput6Data();
-  print('Part 1: ${simulateLanternFishProduction(input6, days: 80)}');
-  print('Part 2: ${simulateOptimizedLanternFishProduction(input6, days: 256)}');
-  print("--------------------------------------------------------");
-
-  print("Day 7 :");
-  final input7 = await getInput7Data();
-  print('Part 1: ${getFuelConsumption(input7)}');
-  print('Part 2: ${getDynamicFuelConsumption(input7)}');
-  print("--------------------------------------------------------");
-
-  print("Day 8 :");
-  final input8 = await getInput8Data();
-  print('Part 1: ${countSimpleDigits(input8)}');
-  print('Part 2: ${sumAllDecodedOutputValues(input8)}');
-  print("--------------------------------------------------------");
-
-  print("Day 9 :");
-  final input9 = await getInput9Data();
-  print('Part 1: ${sumMinHeights(input9)}');
-  print('Part 2: ${getLargestBassinArea(input9)}');
-  print("--------------------------------------------------------");
+  final option = arguments[0].split('=')[0];
+  if (option == '--day') {
+    int day = int.parse(arguments[0].split('=')[1]);
+    return runAdventOfCodeProgram(day: day);
+  }
+  throw Exception('Unknown option $option');
 }
