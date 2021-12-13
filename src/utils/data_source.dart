bf63407922634d4afe70013bac07b8ce0be8ab1c
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2021-12-13 13:47:32 +0100

    :recycle: (utils/data_source) getInputData use getInputDataFromLocation

--- src/utils/data_source.dart
+++ src/utils/data_source.dart
@@ -13,7 +13,14 @@ Future<List<T>> getInputDataFromFile<T>(
       .toList();
 }
 
+Future<List<T>> getInputDataFromLocation<T>(
+    {required int id,
+    required String location,
+    required Parser<T> parser}) async {
+  return getInputDataFromFile(filePath: '$location/input_$id', parser: parser);
+}
+
 Future<List<T>> getInputData<T>(
     {required int id, required Parser<T> parser}) async {
-  return getInputDataFromFile(filePath: 'data/input_$id', parser: parser);
+  return getInputDataFromLocation(id: id, location: 'data', parser: parser);
 }
