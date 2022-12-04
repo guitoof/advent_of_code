ae0311c4011346e00ca309fb8b20c60410ee1b85
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-12-04 16:11:08 +0100

    :bug: (Tests) Remove generic test groups

--- test/challenges/advent_code_0/advent_of_code_0_test.dart
+++ test/challenges/advent_code_0/advent_of_code_0_test.dart
@@ -1,34 +1,35 @@
+import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 import '../../../src/challenges/advent_of_code_0/advent_of_code_0.dart';
 import '../../../src/utils/data_source.dart';
 import '../../test_helpers.dart';
 
 void main() {
-  testSuite<DailySolver0>(
+  dayTestGroup(
     day: 0,
     solverBuilder: () => DailySolver0(
       day: 0,
       name: 'Day 0 - Tester Solver',
     ),
-    part1: PartTestSuite(
-      (part1Solver) {
-        group('[someMethodForPart1]', () {
-          test('should...', () async {
-            /// Expect things here
+    partTestGroups: {
+      1: PartTestGroup(
+        (solver) {
+          final solver0 = castSolverType<DailySolver0>(solver);
+          test('some test from part 1', () {
+            expect(solver0, isA<DailySolver0>());
           });
-        });
-      },
-      skipTypes: [DataSourceType.example, DataSourceType.challenge],
-    ),
-    part2: PartTestSuite(
-      (part1Solver) {
-        group('[someMethodForPart2]', () {
-          test('should...', () async {
-            /// Expect things here
+        },
+        skipTypes: [DataSourceType.challenge],
+      ),
+      2: PartTestGroup(
+        (solver) {
+          final solver0 = castSolverType<DailySolver0>(solver);
+          test('some test from part 2', () {
+            expect(solver0, isA<DailySolver0>());
           });
-        });
-      },
-      skipTypes: [DataSourceType.example, DataSourceType.challenge],
-    ),
+        },
+        skipTypes: [DataSourceType.challenge, DataSourceType.example],
+      ),
+    },
   );
 }
--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -2,11 +2,29 @@ import 'package:test/scaffolding.dart';
 import '../src/utils/daily_solver.dart';
 import '../src/utils/data_source.dart';
 
-class PartTestSuite<S extends DailySolver> {
-  final Function(S solver) block;
+S castSolverType<S extends DailySolver>(DailySolver solver) {
+  assert(solver is S, 'Solver is not of type $S');
+  return solver as S;
+}
+
+void dayTestGroup({
+  required int day,
+  required DailySolver Function() solverBuilder,
+  required Map<int, PartTestGroup> partTestGroups,
+}) {
+  group('Day $day', () {
+    partTestGroups.forEach((key, value) {
+      partTestGroup('Part $key', value.body,
+          solverBuilder: solverBuilder, skipTypes: value.skipTypes);
+    });
+  });
+}
+
+class PartTestGroup {
+  final Function(DailySolver) body;
   final List<DataSourceType> skipTypes;
 
-  PartTestSuite(this.block, {this.skipTypes = const []});
+  PartTestGroup(this.body, {this.skipTypes = const []});
 }
 
 final supportedDataSourceTypes = [
@@ -14,36 +32,26 @@ final supportedDataSourceTypes = [
   DataSourceType.challenge
 ];
 
-void testSuite<S extends DailySolver>({
-  required int day,
-  required S Function() solverBuilder,
-  required PartTestSuite<S> part1,
-  required PartTestSuite<S> part2,
+void partTestGroup(
+  String description,
+  Function(DailySolver solver) body, {
+  required DailySolver Function() solverBuilder,
+  List<DataSourceType> skipTypes = const [],
 }) {
-  final solver = solverBuilder();
-  group('Day $day -', () {
-    group('Part 1 - ', () {
-      for (var type in supportedDataSourceTypes) {
-        group('${type.name.toUpperCase()} - ', () {
-          setUpAll(() async {
-            await solver.loadInputData(ofType: type);
-          });
+  group(description, () {
+    for (var type in supportedDataSourceTypes) {
+      DailySolver solver = solverBuilder();
+      setUp(() {
+        return Future(() async {
+          await solver.loadInputData(ofType: type);
+        });
+      });
 
-          part1.block(solver);
-        }, skip: part1.skipTypes.contains(type));
-      }
-    });
-
-    group('Part 2 - ', () {
-      for (var type in supportedDataSourceTypes) {
-        group('${type.name.toUpperCase()} - ', () {
-          setUpAll(() async {
-            await solver.loadInputData(ofType: type);
-          });
-
-          part2.block(solver);
-        }, skip: part2.skipTypes.contains(type));
-      }
-    });
+      group(
+        '${type.name.toUpperCase()} - ',
+        () => body(solver),
+        skip: skipTypes.contains(type),
+      );
+    }
   });
 }
