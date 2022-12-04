57dea53f6a12472a6aac6592e7770ff5fe0c1bdd
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-12-04 22:32:31 +0100

    :bug: (adventOfCode1 Test) Use testGroupWithExpectedDataByType to fix multi
    type testing

--- test/challenges/advent_code_1/advent_of_code_1_test.dart
+++ test/challenges/advent_code_1/advent_of_code_1_test.dart
@@ -6,62 +6,84 @@ import '../../test_helpers.dart';
 
 void main() {
   dayTestGroup(
-    day: 1,
+    'Day 1',
     solverBuilder: () => DailySolver1(day: 1),
     partTestGroups: {
       1: PartTestGroup(
-        (solver) {
+        (solver, type) {
           final solver1 = castSolverType<DailySolver1>(solver);
-          group('[foodPayloadByElf]', () {
-            test(
-                'should return the list of list of food items carried by each elf',
-                () async {
-              expect(
-                solver1.foodPayloadByElf,
-                equals([
-                  [1000, 2000, 3000],
-                  [4000],
-                  [5000, 6000],
-                  [7000, 8000, 9000],
-                  [10000],
-                ]),
-              );
-            });
-          });
-          group('[getMaxNumberOfCaloriesByElf]', () {
+          testGroupWithExpectedDataByType(
+            '[foodPayloadByElf]',
+            expectedDataMap: {
+              DataSourceType.example: [
+                [1000, 2000, 3000],
+                [4000],
+                [5000, 6000],
+                [7000, 8000, 9000],
+                [10000],
+              ],
+            },
+            type: type,
+            body: ({expectedData}) {
+              test(
+                  'should return the list of list of food items carried by each elf',
+                  () async {
+                expect(
+                  solver1.foodPayloadByElf,
+                  equals(expectedData),
+                );
+              });
+            },
+          );
+          testGroupWithExpectedDataByType('[getMaxNumberOfCaloriesByElf]',
+              expectedDataMap: {
+                DataSourceType.example: 24000,
+                DataSourceType.challenge: 74198,
+              },
+              type: type, body: ({expectedData}) {
             test(
                 'should return the number of calories of the Elf carrying the max calories',
                 () async {
-              expect(await solver1.getMaxNumberOfCaloriesByElf(), 24000);
-            });
-          });
-        },
-        skipTypes: [DataSourceType.challenge],
-      ),
-      2: PartTestGroup(
-        (solver) {
-          final solver1 = castSolverType<DailySolver1>(solver);
-          group('[getTop3MaxNumberOfCalories]', () {
-            test('should return the list of list of the top 3 elves', () async {
               expect(
-                solver1.getTop3MaxNumberOfCalories()..sort(),
-                equals([
-                  24000,
-                  11000,
-                  10000,
-                ]..sort()),
+                await solver1.getMaxNumberOfCaloriesByElf(),
+                expectedData,
               );
             });
           });
-          group('[getTotalTop3MaxNumberOfCalories]', () {
+        },
+        skipTypes: [],
+      ),
+      2: PartTestGroup(
+        (solver, type) {
+          final solver1 = castSolverType<DailySolver1>(solver);
+          testGroupWithExpectedDataByType('[getTop3MaxNumberOfCalories]',
+              expectedDataMap: {
+                DataSourceType.example: [24000, 11000, 10000]..sort(),
+                DataSourceType.challenge: [67758, 67958, 74198]..sort(),
+              },
+              type: type, body: ({expectedData}) {
+            test('should return the list of list of the top 3 elves', () async {
+              expect(
+                solver1.getTop3MaxNumberOfCalories()..sort(),
+                equals(expectedData),
+              );
+            });
+          });
+          testGroupWithExpectedDataByType('[getTotalTop3MaxNumberOfCalories]',
+              expectedDataMap: {
+                DataSourceType.example: 45000,
+                DataSourceType.challenge: 209914,
+              },
+              type: type, body: ({expectedData}) {
             test(
                 'should return the number of calories of the top 3 Elves carrying the max calories',
                 () async {
-              expect(await solver1.getTotalTop3MaxNumberOfCalories(), 45000);
+              expect(await solver1.getTotalTop3MaxNumberOfCalories(),
+                  expectedData);
             });
           });
         },
-        skipTypes: [DataSourceType.challenge, DataSourceType.example],
+        skipTypes: [],
       ),
     },
   );
