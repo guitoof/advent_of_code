749af19e98dba22bf335e2ba6ea41c5cbe8a367c
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-12-04 02:30:39 +0100

    :building_construction: (TestSuite) + possibility to skip example/challenge

--- test/challenges/advent_code_0/advent_of_code_0_test.dart
+++ test/challenges/advent_code_0/advent_of_code_0_test.dart
@@ -1,26 +1,34 @@
 import 'package:test/scaffolding.dart';
 import '../../../src/challenges/advent_of_code_0/advent_of_code_0.dart';
+import '../../../src/utils/data_source.dart';
 import '../../test_helpers.dart';
 
 void main() {
   testSuite<DailySolver0>(
+    day: 0,
+    solverBuilder: () => DailySolver0(
       day: 0,
-      solverBuilder: () => DailySolver0(
-            day: 0,
-            name: 'Day 0 - Tester Solver',
-          ),
-      part1TestBlock: (part1Solver) {
+      name: 'Day 0 - Tester Solver',
+    ),
+    part1: PartTestSuite(
+      (part1Solver) {
         group('[someMethodForPart1]', () {
           test('should...', () async {
             /// Expect things here
           });
         });
       },
-      part2TestBlock: (part2Solver) {
+      skipTypes: [DataSourceType.example, DataSourceType.challenge],
+    ),
+    part2: PartTestSuite(
+      (part1Solver) {
         group('[someMethodForPart2]', () {
           test('should...', () async {
             /// Expect things here
           });
         });
-      });
+      },
+      skipTypes: [DataSourceType.example, DataSourceType.challenge],
+    ),
+  );
 }
--- test/test_helpers.dart
+++ test/test_helpers.dart
@@ -2,35 +2,47 @@ import 'package:test/scaffolding.dart';
 import '../src/utils/daily_solver.dart';
 import '../src/utils/data_source.dart';
 
+class PartTestSuite<S extends DailySolver> {
+  final Function(S solver) block;
+  final List<DataSourceType> skipTypes;
+
+  PartTestSuite(this.block, {this.skipTypes = const []});
+}
+
+final supportedDataSourceTypes = [
+  DataSourceType.example,
+  DataSourceType.challenge
+];
+
 void testSuite<S extends DailySolver>({
   required int day,
   required S Function() solverBuilder,
-  required Function(S solver) part1TestBlock,
-  required Function(S solver) part2TestBlock,
+  required PartTestSuite<S> part1,
+  required PartTestSuite<S> part2,
 }) {
   final solver = solverBuilder();
   group('Day $day -', () {
     group('Part 1 - ', () {
-      for (var type in [DataSourceType.example, DataSourceType.challenge]) {
+      for (var type in supportedDataSourceTypes) {
         group('${type.name.toUpperCase()} - ', () {
           setUpAll(() async {
             await solver.loadInputData(ofType: type);
           });
 
-          part1TestBlock(solver);
-        }, skip: type != DataSourceType.example);
+          part1.block(solver);
+        }, skip: part1.skipTypes.contains(type));
       }
     });
 
     group('Part 2 - ', () {
-      for (var type in [DataSourceType.example, DataSourceType.challenge]) {
+      for (var type in supportedDataSourceTypes) {
         group('${type.name.toUpperCase()} - ', () {
           setUpAll(() async {
             await solver.loadInputData(ofType: type);
           });
 
-          part2TestBlock(solver);
-        }, skip: type != DataSourceType.example);
+          part2.block(solver);
+        }, skip: part2.skipTypes.contains(type));
       }
     });
   });
