6ed19c7f748363d186f0a3b92eac881560c5f8dd
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2022-12-11 17:00:03 +0100

    :sparkles: (Day 5 - Part 1)

---
+++ src/challenges/advent_of_code_5/test/advent_of_code_5_test.dart
@@ -0,0 +1,118 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import '../advent_of_code_5.dart';
+import '../../../utils/data_source.dart';
+import '../../../utils/test/test_helpers.dart';
+
+void main() {
+  dayTestGroup(
+    'Day 5',
+    solverBuilder: () => DailySolver5(day: 5),
+    partTestGroups: {
+      1: PartTestGroup(
+        (solver, type) {
+          final solver5 = castSolverType<DailySolver5>(solver);
+          group(
+            'CargoBay',
+            () {
+              testGroupWithExpectedDataByType(
+                '[loadFromDrawing]',
+                expectedDataMap: {
+                  DataSourceType.example: {
+                    'input': [
+                      '    [D]    ',
+                      '[N] [C]    ',
+                      '[Z] [M] [P]',
+                      '  1   2   3 '
+                    ],
+                    'output': CargoBay.ofStacksCount(3)
+                      ..loadCrateToStack(Crate(label: 'Z'), 1)
+                      ..loadCrateToStack(Crate(label: 'M'), 2)
+                      ..loadCrateToStack(Crate(label: 'P'), 3)
+                      ..loadCrateToStack(Crate(label: 'N'), 1)
+                      ..loadCrateToStack(Crate(label: 'C'), 2)
+                      ..loadCrateToStack(Crate(label: 'D'), 2)
+                  },
+                },
+                type: type,
+                body: ({expectedData}) {
+                  test(
+                      'should parse the raw drawing into a fully loaded [CargoBay]',
+                      () async {
+                    expect(CargoBay.loadFromDrawing(expectedData['input']),
+                        expectedData['output']);
+                  });
+                },
+              );
+            },
+          );
+          testGroupWithExpectedDataByType(
+            '[getInitialTopCratesLabels]',
+            expectedDataMap: {
+              DataSourceType.example: 'NDP',
+            },
+            type: type,
+            body: ({expectedData}) {
+              test(
+                  'should return the labels of all the top cranes as laid out initially',
+                  () async {
+                expect(solver5.getInitialTopCratesLabels(), expectedData);
+              });
+            },
+          );
+          testGroupWithExpectedDataByType(
+            '[rearrangementProcedure]',
+            expectedDataMap: {
+              DataSourceType.example:
+                  '[Move (1) from (2) to (1), Move (3) from (1) to (3), Move (2) from (2) to (1), Move (1) from (1) to (2)]',
+            },
+            type: type,
+            body: ({expectedData}) {
+              test('should return the parsed rearrangement procedure',
+                  () async {
+                expect(solver5.rearrangementProcedure.toString(), expectedData);
+              });
+            },
+          );
+          testGroupWithExpectedDataByType(
+            '[getRearrangedTopCratesLabels]',
+            expectedDataMap: {
+              DataSourceType.example: 'CMZ',
+            },
+            type: type,
+            body: ({expectedData}) {
+              test(
+                  'should return the labels of all the top cranes after rearrangement',
+                  () async {
+                expect(solver5.getRearrangedTopCratesLabels(), expectedData);
+              });
+            },
+          );
+        },
+        skipTypes: [DataSourceType.challenge],
+      ),
+      // 2: PartTestGroup(
+      //   (solver, type) {
+      //     final solver5 = castSolverType<DailySolver5>(solver);
+      //     testGroupWithExpectedDataByType(
+      //       '[someMethod2] relevant to part 2',
+      //       expectedDataMap: {
+      //         DataSourceType.example: 'Expected Data for Example',
+      //         // Challenge case will be skipped
+      //         // if [DataSourceType.challenge] is missing (null)
+      //       },
+      //       type: type,
+      //       body: ({expectedData}) {
+      //         test('test some behavior of [someMethod2]', () async {
+      //           expect(solver5, isA<DailySolver5>());
+      //           // Use [expectedData] to test the behavior of [someMethod]
+      //           // expect(solver5.someMethod2(), expectedData);
+      //         });
+      //       },
+      //     );
+      //   },
+      //   skipTypes: [DataSourceType.challenge, DataSourceType.example],
+      // ),
+    },
+  );
+}
